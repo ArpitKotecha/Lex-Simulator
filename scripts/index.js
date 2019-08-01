add3e3a3c76aab45342de653c67361031b4dd5e4
jatin-rathee <jatinrathee2018@gmail.com>
2019-08-01 18:24:44 +0530

    Added Links and more forms

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,53 @@
-document.querySelectorAll(".arrow_right_btn")[0].addEventListener("click", function () {
+function open1() {
+    document.querySelector(".modal2").style.display = "none";
+    document.querySelector(".modal1").style.display = "block";
+    document.querySelector(".modal3").style.display = "none";
+    document.querySelector(".modal4").style.display = "none";
+    document.querySelector(".modal5").style.display = "none";
+    document.querySelector(".modal6").style.display = "none";
+}
+
+function open2() {
     document.querySelector(".modal1").style.display = "none";
     document.querySelector(".modal2").style.display = "block";
-})
+    document.querySelector(".modal3").style.display = "none";
+    document.querySelector(".modal4").style.display = "none";
+    document.querySelector(".modal5").style.display = "none";
+    document.querySelector(".modal6").style.display = "none";
+}
 
-document.querySelectorAll(".arrow_left_btn")[1].addEventListener("click", function () {
-    document.querySelector(".modal1").style.display = "block";
+function open3() {
+    document.querySelector(".modal1").style.display = "none";
+    document.querySelector(".modal3").style.display = "block";
     document.querySelector(".modal2").style.display = "none";
-})
+    document.querySelector(".modal4").style.display = "none";
+    document.querySelector(".modal5").style.display = "none";
+    document.querySelector(".modal6").style.display = "none";
+}
+
+function open4() {
+    document.querySelector(".modal1").style.display = "none";
+    document.querySelector(".modal4").style.display = "block";
+    document.querySelector(".modal3").style.display = "none";
+    document.querySelector(".modal2").style.display = "none";
+    document.querySelector(".modal5").style.display = "none";
+    document.querySelector(".modal6").style.display = "none";
+}
+
+function open5() {
+    document.querySelector(".modal1").style.display = "none";
+    document.querySelector(".modal5").style.display = "block";
+    document.querySelector(".modal3").style.display = "none";
+    document.querySelector(".modal4").style.display = "none";
+    document.querySelector(".modal2").style.display = "none";
+    document.querySelector(".modal6").style.display = "none";
+}
+
+function open6() {
+    document.querySelector(".modal1").style.display = "none";
+    document.querySelector(".modal6").style.display = "block";
+    document.querySelector(".modal3").style.display = "none";
+    document.querySelector(".modal4").style.display = "none";
+    document.querySelector(".modal5").style.display = "none";
+    document.querySelector(".modal2").style.display = "none";
+}
